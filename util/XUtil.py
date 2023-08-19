c094375755c7db15e168345f29bf3c4dc1dd52e6
PythonnotJava <88701385+PythonnotJava@users.noreply.github.com>
2023-08-19 14:56:21 +0800

    Update XUtil.py

--- util/XUtil.py
+++ util/XUtil.py
@@ -1,18 +1,15 @@
-# 这里放置了核心功能
-from util._XUtil import changeTalents
-from util.UserObject import UserAttr
+# Core utility classes
+from util import changeTalents, UserAttr, PetAttr
 from os import PathLike
 from typing import Union, Optional, Generator
-from CommonConst.GlobalConstant import ORIGINAL_TALENT_CHANGE_COST, HIGH_LEVEL_TALENT_CHANGE_COST
-from util.PetObject import PetAttr
+from CommonConst import ORIGINAL_TALENT_CHANGE_COST, HIGH_LEVEL_TALENT_CHANGE_COST
 
-# 实现多个精灵文件对应同一个用户，而不是每个精灵都对应一个用户
-# 否则，会出现每个精灵单独读取用户资源而导致写入混乱问题
-# 所以，每次精灵系统运作的时候，只能对应一个全局用户
+# Implemente multiple elf datas for the same user, rather than each elf data for one user
+# so, when the system is running, the user have had prepended.
 class UserCoreXUtil(UserAttr):
     def __init__(self, userInfo_path: Union[str, PathLike[str]]):
         super().__init__()
-        # 连接用户
+        # connect a user
         self.userInfo_path = userInfo_path
         self.ReadObjectFile(userInfo_path)
 
@@ -37,15 +34,17 @@ class UserCoreXUtil(UserAttr):
 class PetTalentCoreXUtil:
 
     def __init__(self, petInfo_path : Union[str, PathLike[str]]):
-        # 连续失败改造次数
+        # the counts of talents-modification
         self.successive_failures_talents_change_times : int = 0
-        # 连接精灵
+        # connect an elf and get it's infomation
         self.relevantPet = PetAttr()
         self.petInfo_path = petInfo_path
         self.relevantPet.ReadObjectFile(petInfo_path)
         self.relevantPet_info : dict = dict(self.relevantPet)
-        # 当前的天赋值列表、新变化的天赋值列表、变化的天赋值列表
-        # 当前的天赋值列表的镜像、新变化的天赋值列表的镜像、变化的天赋值列表的镜像，这三个是用于取消恢复
+        # current list of each talent attriibute
+        # newly changed list of each talent attriibute
+        # changed values of list of each talent attriibute
+        # mirrors of three list above, used for recovery
         self.current_list : list = self.relevantPet_info['talent_atrrs']
         self.new_list = []
         self.change_list = []
@@ -55,14 +54,16 @@ class PetTalentCoreXUtil:
 
         self.generatorToList : list = []
 
-    # 1.获取...
-    # 2.在每次开启天赋系统时，有以下算法
-    # - 每个精灵每5次降低必定在第六次提升，第六次指的是普通改造第六次
-    # 3.视变化值为0时属于提升的情况
+    # The function performance
+    # 1.get ...
+    # 2.optimize the algorithm named changeTalents(you can see its implementation in _XUtil.py)
+    # - if using ordinary talent-modifacation, once the current fails, it will be counted,
+    #   when counts equals 5, the talent-modifacation will succed next time.
+    # 3.consider changed values which is equal 0 as success
     def __getComponent(self, mode: bool) -> None:
         _data: Generator = changeTalents(self.current_list, mode)
         print("Base on self.current_list --> ", self.current_list)
-        # generatorToList是用于存放每次生成器转换为列表的属性
+        # generatorToList is a list used to convert generator to list
         self.generatorToList = list(_data)
         self.current_list_mirror : list = self.current_list.copy()
         self.new_list_mirror = self.new_list.clear()
@@ -83,26 +84,26 @@ class PetTalentCoreXUtil:
         print('change_list', self.change_list)
 
     def getComponent(self, mode : bool) -> None:
-        # 普通改造
+        # ordinary talent-modification
         if mode:
-            # 连续失败,但是概率比较小，于是选择每累计五次为准
+            # if failure-counts is equal 5, next time talent-modification is sure to succed.
             if self.successive_failures_talents_change_times == 5:
                 self.__getComponent(False)
                 self.successive_failures_talents_change_times = 0
             else:
                 self.__getComponent(True)
-                # 降低属性了
+                # low the attribute
                 if sum(self.change_list) < 0:
                     self.successive_failures_talents_change_times += 1
                 else:
-                    # 本来想写连续失败，但是概率比较小，于是选择每累计五次为准
+                    # if want to realize continuous talent-modification-next-time-lift, you can add this code below
                     # self.successive_failures_talents_change_times = 0
                     ...
-        # 高级改造
+        # high-level reconstruction
         else:
             self.__getComponent(False)
 
-    # 星级提升/降低功能还有下一个星级的距离
+    # rewrite JudgeElfStarAndDistance
     def reWriteJudgeElfStarAndDistance(self) -> tuple[int, int]:
         _value = sum(self.current_list)
         if _value == 0:
@@ -122,27 +123,25 @@ class PetTalentCoreXUtil:
         else:
             return 0, 0
 
-    # 每次改造都需要花费
-    # 这里处理花费问题
-    # 使用哪种改造，就会对相关货币进行响应，mode参数为True表明采用普通改造
-    # 返回包含状态码，请到全局变量查询
+    # every modifocation needs money/gold
+    # if succeds, reduce relevant counts
+    # return status-code
     def costByRelevantCurrencyAndCheckIt(self,
-                                         # 连接用户
+                                         # connect the user
                                          userCoreXUtil : Optional[UserCoreXUtil] = None,
                                          mode : bool = True,
                                          cost1 : int = ORIGINAL_TALENT_CHANGE_COST,
                                          cost2 : int = HIGH_LEVEL_TALENT_CHANGE_COST
                                          ) -> int:
-        # 连接用户前提下
+        # if connects a user
         if userCoreXUtil is not None:
             if mode:
-                # 钱够
+                # if gold is enough
                 if userCoreXUtil.gold >= cost1:
                     self.getComponent(True)
                     userCoreXUtil.ModifyOneItem('gold', -cost1)
                     # userCoreXUtil.gold -= cost1
                     return 3
-                # 钱不够
                 else:
                     return 1
             else:
@@ -155,21 +154,19 @@ class PetTalentCoreXUtil:
                     return 2
         else:
             return 13
-    # save : 为True则保存，反之（保存或者取消保存按钮功能，UI思路是先因此天赋改造按钮，如何展现这两个按钮以及链接功能，切换来回）
-    # 保存的情况下，原来的各个属性列表，会被新的属性列表代替
-    # 前提是状态码为3
+    # save : if saved, renew the list
+    # on condition that the status-code is equal 3
     def save(self, save : bool = True):
         if save:
             self.current_list_mirror = self.current_list
             self.current_list = self.new_list.copy()
         else:
             self.current_list = self.current_list_mirror.copy()
-
-    # 把改造（被读入也算，这一步完全可以优化，请看核心UI的思路，虽然没实现）的精灵信息写回
     def writeBackFileStream(self,
                             writeBackPath : Union[Optional[str], PathLike[str]] = None,
                             encoding: str = 'U8'):
-        # 不传新路径就覆盖，其实本来就是这样，传入新路径是测试用的
+        # original file(s) will be covered if not income path
+        # Of course, we shouldn't income path indeed. It's just a test for incoming path to check if relevnt codes run.
         self.relevantPet_info['talent_atrrs'] = self.current_list
         self.relevantPet.WriteBackObjectFile(self.relevantPet_info,
                                              writeBackPath if writeBackPath is not None else self.petInfo_path,
